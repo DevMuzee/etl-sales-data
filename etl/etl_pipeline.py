229b4d107955067974dc7ff53f21f3c71dcf8b78
Abdulmuiz Adewale <abdulmuiz.adewale@tolaram.com>
2025-09-24 14:04:22 +0100

    total_sales column added

--- etl/etl_pipeline.py
+++ etl/etl_pipeline.py
@@ -15,9 +15,13 @@ url_repo.columns= [col.replace(' ', '_') for col in url_repo.columns]
 #---filling the null values in price with mean value of price
 url_repo['price'].fillna(url_repo['price'].mean(), inplace=True)
 
-print(url_repo.isna().sum())
+#Adding a new column total_sales
+url_repo['total_sales']= url_repo['quantity'] * url_repo['price']
+url_repo['total_sales']= url_repo['total_sales'].round(2)
+#url_repo['total_sales']= url_repo['total_sales'].map('{:.2f}'.format).astype(float)
+print(url_repo.head())
 
 #saved the cleaned data to local repository
 url_repo.to_csv(r"C:/Users/abdulmuiz.adewale/postgresql/git_collab/etl-sales-data/data/cleaned_sales.csv", index=False)
-print(f'Extracted {len(url_repo)} rows and {len(url_repo.columns)} columns and \n ETL process is completed')
+print(f'Extracted {len(url_repo)} rows and {len(url_repo.columns)} columns and \nETL process is completed')
 
