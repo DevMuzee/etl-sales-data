b7fc6805743586cb12d584659bb1d30f73b963e0
Abdulmuiz Adewale <abdulmuiz.adewale@tolaram.com>
2025-09-24 12:39:38 +0100

    Extraction||Transformation(lowercase|fillna)||Loading

---
+++ etl/etl_pipeline.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+#Extraction pipeline
+print('Data is gotten from local repository...........')
+url_repo= pd.read_csv(r"C:/Users/abdulmuiz.adewale/postgresql/git_collab/etl-sales-data/data/raw_sales.csv")
+print(f"Extracted {len(url_repo)} rows and {len(url_repo.columns)} columns")
+
+#Transformation and cleaning pipelines
+
+#---changing the column to lower cases and adding underscore in place of spaces
+url_repo.columns= [col.lower() for col in url_repo.columns]
+url_repo.columns= [col.replace(' ', '_') for col in url_repo.columns]
+
+#---filling the null values in price with mean value of price
+url_repo['price'].fillna(url_repo['price'].mean(), inplace=True)
+
+print(url_repo.isna().sum())
+
+#saved the cleaned data to local repository
+url_repo.to_csv(r"C:/Users/abdulmuiz.adewale/postgresql/git_collab/etl-sales-data/data/cleaned_sales.csv", index=False)
+print(f'Extracted {len(url_repo)} rows and {len(url_repo.columns)} columns and \n ETL process is completed')
+
